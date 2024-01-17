6e15143b1b907df334f1b3d994fac214357f87e3
PyTrainee <eternal@disroot.org>
2024-01-17 03:00:24 +0200

    slick test2

--- carousel-slick-one.js
+++ carousel-slick-one.js
@@ -1,7 +1,50 @@
-$('.carousel-slick').slick({
-    dots: true,
-    infinite: true,
-    speed: 300,
-    slidesToShow: 6,
-    slidesToScroll: 1
+function myFunction(x) {
+    if (x.matches) { // If media query matches
+        $('.carousel-slick').slick({
+            dots: false,
+            infinite: true,
+            speed: 300,
+            slidesToShow: 6,
+            slidesToScroll: 1
+        });
+    } else {
+            $('.carousel-slick').slick({
+                dots: false,
+                infinite: true,
+                speed: 300,
+                slidesToShow: 1,
+                slidesToScroll: 1
+            });
+    }
+}
+
+// Create a MediaQueryList object
+var x = window.matchMedia("(min-width: 900px)")
+
+// Call listener function at run time
+myFunction(x);
+
+// Attach listener function on state changes
+x.addEventListener("change", function() {
+    myFunction(x);
 });
+
+/*
+let aaa = window.innerWidth;
+if (aaa >= 900) {
+    $('.carousel-slick').slick({
+        dots: false,
+        infinite: true,
+        speed: 300,
+        slidesToShow: 6,
+        slidesToScroll: 1
+    });
+} else {
+    $('.carousel-slick').slick({
+        dots: false,
+        infinite: true,
+        speed: 300,
+        slidesToShow: 1,
+        slidesToScroll: 1
+    });
+}*/
