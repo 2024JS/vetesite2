a9f84d62e06fc20a55b844a96a519b130d6dee8c
PyTrainee <eternal@disroot.org>
2024-01-19 11:30:28 +0200

    return

--- carousel-slick-orange.js
+++ carousel-slick-orange.js
@@ -1,8 +1,66 @@
-$('.carousel-slick-orange').slick({
-    dots: false,
-    infinite: true,
-    speed: 300,
-    slidesToShow: 6,
-    slidesToScroll: 1
+function myFunction(x) {
+    if (x.matches) { // If media query matches
+        $('.carousel-slick-orange').slick({
+            dots: false,
+            infinite: true,
+            speed: 300,
+            slidesToShow: 6,
+            slidesToScroll: 1
+            
+        });
+    } else {
+            $('.carousel-slick-orange').slick({
+                dots: false,
+                infinite: true,
+                speed: 300,
+                slidesToShow: 1,
+                slidesToScroll: 1
+            });
+    }
+}
 
+// Create a MediaQueryList object
+var x = window.matchMedia("(min-width: 900px)")
+
+// Call listener function at run time
+myFunction(x);
+
+// Attach listener function on state changes
+x.addEventListener("change", function() {
+    myFunction(x);
+    
+    
 });
+
+
+/*console.log(document.querySelectorAll(".carousel_tr_p .carousel-slick__container").length)
+
+let divv = document.querySelector(".carousel-slick");
+const news = divv.closest("div")
+console.log(news)
+
+
+console.log(document.querySelector(".carousel-slick").closest("div"))
+console.log(news.search([carousel]));*/
+
+
+
+/*
+let aaa = window.innerWidth;
+if (aaa >= 900) {
+    $('.carousel-slick').slick({
+        dots: false,
+        infinite: true,
+        speed: 300,
+        slidesToShow: 6,
+        slidesToScroll: 1
+    });
+} else {
+    $('.carousel-slick').slick({
+        dots: false,
+        infinite: true,
+        speed: 300,
+        slidesToShow: 1,
+        slidesToScroll: 1
+    });
+}*/
--- carousel-slick-trend.js
+++ carousel-slick-trend.js
@@ -1,8 +1,34 @@
-$('.carousel-slick-trend').slick({
-    dots: false,
-    infinite: true,
-    speed: 300,
-    slidesToShow: 4,
-    slidesToScroll: 1
+function myFunction(x) {
+    if (x.matches) { // If media query matches
+        $('.carousel-slick-trend').slick({
+            dots: false,
+            infinite: true,
+            speed: 300,
+            slidesToShow: 4,
+            slidesToScroll: 1
+
+        });
+    } else {
+        $('.carousel-slick-trend').slick({
+            dots: false,
+            infinite: true,
+            speed: 300,
+            slidesToShow: 1,
+            slidesToScroll: 1
+        });
+    }
+}
+
+
+// Create a MediaQueryList object
+var x = window.matchMedia("(min-width: 900px)")
+
+// Call listener function at run time
+myFunction(x);
+
+// Attach listener function on state changes
+x.addEventListener("change", function() {
+    myFunction(x);
+
 
 });
