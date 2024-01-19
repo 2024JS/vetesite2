a7ad5d1f07c254412a3deff8a854cfdf776169af
PyTrainee <eternal@disroot.org>
2024-01-19 08:46:13 +0200

    start do footer

---
+++ carousel-slick-testimonials.js
@@ -0,0 +1,34 @@
+function myFunction(x) {
+    if (x.matches) { // If media query matches
+        $('.carousel-slick-testimonials').slick({
+            dots: false,
+            infinite: true,
+            speed: 300,
+            slidesToShow: 3,
+            slidesToScroll: 1
+
+        });
+    } else {
+        $('.carousel-slick-orange').slick({
+            dots: false,
+            infinite: true,
+            speed: 300,
+            slidesToShow: 1,
+            slidesToScroll: 1
+        });
+    }
+}
+
+// Create a MediaQueryList object
+var x = window.matchMedia("(min-width: 900px)")
+
+// Call listener function at run time
+myFunction(x);
+
+// Attach listener function on state changes
+x.addEventListener("change", function() {
+    myFunction(x);
+
+
+});
+
